c7d1f3fb0ec887fc0be4e900f5682a79ca2d118e
Jason Davey <jason.davey@myverascore.com>
2025-02-10 09:50:13 -0500

    Extended input using command args

--- src/plaidSyncWorkflow.ts
+++ src/plaidSyncWorkflow.ts
@@ -1,26 +1,37 @@
 import { assign, fromPromise, setup } from 'xstate';
 import { getGreeting } from '.';
+import { Client } from 'fauna';
+
+const ownerId = process.argv[2];
+const clientId = process.argv[3];
 
 export const plaidAccountSyncWorfklow = setup({
   types: {
     context: {} as {
+      ownerId: string;
+      clientId: string;
       name: string;
       data: {
         greeting: string;
       } | null;
+      parentDbConnection: Client | null;
     }
   },
   actors: {
-    fetchUser: fromPromise(({ input }: { input: { name: string } }) =>
-      getGreeting(input.name)
+    fetchUser: fromPromise(
+      ({ input }: { input: { name: string; ownerId: string } }) =>
+        getGreeting(input.ownerId)
     )
   }
 }).createMachine({
   initial: 'idle',
   id: 'plaid_sync',
   context: {
+    ownerId,
+    clientId,
     name: 'World',
-    data: null
+    data: null,
+    parentDbConnection: null
   },
   states: {
     idle: {
@@ -31,7 +42,10 @@ export const plaidAccountSyncWorfklow = setup({
     loading: {
       invoke: {
         src: 'fetchUser',
-        input: ({ context }) => ({ name: context.name }),
+        input: ({ context }) => ({
+          name: context.name,
+          ownerId: context.ownerId
+        }),
         onDone: {
           target: 'success',
           actions: assign({
